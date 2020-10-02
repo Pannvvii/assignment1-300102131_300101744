92fa0a1d933e12f22a2a3b7911672731359d732a
Pierre Allard <palla036@uottawa.ca>
2020-10-02 17:49:46 -0400

    added data for iteration

--- pointcp/ARRAY.md
+++ pointcp/ARRAY.md
@@ -39,3 +39,49 @@ Vector miliseconds: 1571
 Vector seconds: 1
 Array miliseconds: 1664
 Array seconds: 1
+
+LinkedList Creation miliseconds: 19
+LinkedList Iteration miliseconds: 11249
+LinkedList C/I seconds: 0/11
+Vector Creation miliseconds: 24
+Vector Iteration miliseconds: 10758
+Vector C/I seconds: 0/10
+Array Creation miliseconds: 22
+Array Iteration miliseconds: 10954
+Array C/I seconds: 0/10
+LinkedList Creation miliseconds: 34
+LinkedList Iteration miliseconds: 12284
+LinkedList C/I seconds: 0/12
+Vector Creation miliseconds: 13
+Vector Iteration miliseconds: 11016
+Vector C/I seconds: 0/11
+Array Creation miliseconds: 8
+Array Iteration miliseconds: 10640
+Array C/I seconds: 0/10
+LinkedList Creation miliseconds: 6
+LinkedList Iteration miliseconds: 11315
+LinkedList C/I seconds: 0/11
+Vector Creation miliseconds: 11
+Vector Iteration miliseconds: 10629
+Vector C/I seconds: 0/10
+Array Creation miliseconds: 8
+Array Iteration miliseconds: 10640
+Array C/I seconds: 0/10
+LinkedList Creation miliseconds: 5
+LinkedList Iteration miliseconds: 11170
+LinkedList C/I seconds: 0/11
+Vector Creation miliseconds: 9
+Vector Iteration miliseconds: 10657
+Vector C/I seconds: 0/10
+Array Creation miliseconds: 8
+Array Iteration miliseconds: 10634
+Array C/I seconds: 0/10
+LinkedList Creation miliseconds: 5
+LinkedList Iteration miliseconds: 11135
+LinkedList C/I seconds: 0/11
+Vector Creation miliseconds: 9
+Vector Iteration miliseconds: 10704
+Vector C/I seconds: 0/10
+Array Creation miliseconds: 9
+Array Iteration miliseconds: 11096
+Array C/I seconds: 0/11
--- pointcp/ArrayTesting/ArrayTesting.java
+++ pointcp/ArrayTesting/ArrayTesting.java
@@ -1,49 +1,75 @@
 import java.util.Random;
 import java.util.*;
+import java.util.Vector;
 public class ArrayTesting {
     
     public static void main(String[] args){
-        long timl = System.nanoTime();
-        Random rand = new Random();
-        int n = 80000000;
+        for (int m= 0; m<5;m++){
+            Random rand = new Random();
+            String listconc = "";
+            String veconc = "";
+            String arronc = "";
+            int n = 450000;
 
-        String[] alph = {"a","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+            long start1 = System.nanoTime();
+            String[] alph = {"a","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+            LinkedList<String> link = new LinkedList<String>();
+            for (int i = 0; i<n;i++){
+                int randy = rand.nextInt(25);
+                link.add(alph[randy]);
+            }
+            long middle1 = System.nanoTime();
+            long middle12 = System.nanoTime();
+            Iterator check1 = link.iterator();
+            while(check1.hasNext()){
+                listconc = listconc+check1.next();
+            }
+            long end1 = System.nanoTime();
+            long createTime1 = (middle1-start1)/1000000;
+            long iterateTime1 = (end1 - middle12)/1000000;
+            System.out.println("LinkedList Creation miliseconds: "+createTime1);
+            System.out.println("LinkedList Iteration miliseconds: "+iterateTime1);
+            System.out.println("LinkedList C/I seconds: "+createTime1/1000+"/"+iterateTime1/1000);
+            
 
-        LinkedList<String> link = new LinkedList<String>();
-        for (int i = 0; i<n;i++){
-            int randy = rand.nextInt(25);
-            link.add(alph[randy]);
+            long start2 = System.nanoTime();
+            Vector<String> vect = new Vector<String>(n);
+            for (int i = 0; i<n;i++){
+                int randv = rand.nextInt(25);
+                vect.add(alph[randv]);
+            }
+            long middle2 = System.nanoTime();
+            long middle22 = System.nanoTime();
+            Iterator check = vect.iterator();
+            while (check.hasNext()){
+                veconc = veconc+check.next();
+            }
+            long end2 = System.nanoTime();
+            long createTime2 = (middle2-start2)/1000000;
+            long iterateTime2 = (end2 - middle22)/1000000;
+            System.out.println("Vector Creation miliseconds: "+createTime2);
+            System.out.println("Vector Iteration miliseconds: "+iterateTime2);
+            System.out.println("Vector C/I seconds: "+createTime2/1000+"/"+iterateTime2/1000);
+
+
+            long start3 = System.nanoTime();
+            String arry[] = new String[n];
+            for (int i = 0; i<n;i++){
+                int randa = rand.nextInt(25);
+                arry[i] = alph[randa];
+            }
+            long middle3 = System.nanoTime();
+            long middle32 = System.nanoTime();
+            for (int i = 0; i<n;i++){
+                arronc = arronc+arry[i];
+            }
+            long end3 = System.nanoTime();
+            long createTime3 = (middle3-start3)/1000000;
+            long iterateTime3 = (end3 - middle32)/1000000;
+            System.out.println("Array Creation miliseconds: "+createTime3);
+            System.out.println("Array Iteration miliseconds: "+iterateTime3);
+            System.out.println("Array C/I seconds: "+createTime3/1000+"/"+iterateTime3/1000);
         }
-        long timel = System.nanoTime();
-        long milisecl = (timel-timl)/1000000;
-        long secl = milisecl/1000;
-        System.out.println("LinkedList miliseconds: "+milisecl);
-        System.out.println("LinkedList seconds: "+secl);
-        
-
-        long timv = System.nanoTime();
-        Vector<String> vect = new Vector<String>(n);
-        for (int i = 0; i<n;i++){
-            int randv = rand.nextInt(25);
-            vect.add(alph[randv]);
-        }
-        long timev = System.nanoTime();
-        long milisecv = (timev-timv)/1000000;
-        long secv = milisecv/1000;
-        System.out.println("Vector miliseconds: "+milisecv);
-        System.out.println("Vector seconds: "+secv);
-
-        long tima = System.nanoTime();
-        String arry[] = new String[n];
-        for (int i = 0; i<n;i++){
-            int randa = rand.nextInt(25);
-            arry[i] = alph[randa];
-        }
-        long timea = System.nanoTime();
-        long miliseca = (timea-tima)/1000000;
-        long seca = miliseca/1000;
-        System.out.println("Array miliseconds: "+miliseca);
-        System.out.println("Array seconds: "+seca);
     }
     
 }
